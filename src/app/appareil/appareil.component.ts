623ebe561315eebe1d37508b59e15c7573981d98
m33erick <m33erick@gmail.com>
2019-01-13 18:31:34 +0100

    ajout

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class AppareilComponent implements OnInit {
 
   @Input() appareilName: string;
-  @Input() appareilStatus: string'
+  @Input() appareilStatus: string;
 
   constructor() { }
 
